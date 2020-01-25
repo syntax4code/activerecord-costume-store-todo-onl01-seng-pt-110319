910126f65c63d62454fc21fe64fb831380b992fa
Warren Harper II <warrenharper2@att.net>
2020-01-25 22:39:08 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class Costume < ActiveRecord::Base
   class CreateCostumes < ActiveRecord::Migration
-    def change 
+    def change
       create_table :costumes do |t|
         t.string :name
         t.float :price
