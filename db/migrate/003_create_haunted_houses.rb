bc7d820605529cbbe4e639736e795f6ea00c1cb6
Warren Harper II <warrenharper2@att.net>
2020-01-25 22:40:42 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,15 @@
 # Create your haunted_houses migration here
-class HauntedHouse < ActiveRecord::Base
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+  create_table :haunted_houses do |t|
+    t.string :name
+    t.string :location
+    t.string :theme
+    t.float :price
+    t.boolean :family_friendly
+    t.datetime :opening_date
+    t.datetime :closing_date
+    t.text :description
+  end
+end
 end
