d5c943a5f5c546b16d554e512f130950ca26b2f8
Warren Harper II <warrenharper2@att.net>
2020-01-25 23:32:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,16 +1,14 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores < ActiveRecord::Migration
-  def change
-   create_table :costume_stores do |t|
-     t.string :name
-     t.string :location
-     t.integer :costume_inventory
-     t.integer :num_of_employees
-     t.boolean :still_in_business
-     t.datetime :opening_time
-     t.datetime :closing_time
-   end
- end
-end
-end
+endclass Costume < ActiveRecord::Base
+  class CreateCostumes < ActiveRecord::Migration
+    def change
+      create_table :costumes do |t|
+        t.string :name
+        t.float :price
+        t.string :image_url
+        t.string :size
+        t.timestamps
+      end
+    end
+  end
